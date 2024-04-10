3131ce2a7c1372750695eced4cc812dff4ea2813
anjianshi <anjianshi@gmail.com>
2024-04-10 23:03:42 +0800

    优化发布流程

---
+++ utils/publish-prepare.cjs
@@ -0,0 +1,16 @@
+/* eslint-env node */
+const fs = require('node:fs')
+const path = require('node:path')
+
+const base = path.resolve(__dirname, './')
+const dist = path.join(base, 'dist')
+
+const packageJSON = require(path.join(base, 'package.json'))
+delete packageJSON.publishConfig.directory
+for (const [key, value] of Object.entries(packageJSON.dependencies)) {
+  packageJSON.dependencies[key] = value.replace('workspace:', 'workspace:../')
+}
+for (const [key, value] of Object.entries(packageJSON.devDependencies)) {
+  packageJSON.devDependencies[key] = value.replace('workspace:', 'workspace:../')
+}
+fs.writeFileSync(path.join(dist, 'package.json'), JSON.stringify(packageJSON, null, 2))
