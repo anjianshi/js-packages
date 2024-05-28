71a99685195fc9bfcb4de90cbf76af65b993a801
anjianshi <anjianshi@gmail.com>
2024-05-28 17:32:27 +0800

    env-reader 支持不带默认值取值，修正 boolean 值转换

--- utils/src/env-node/env-reader.ts
+++ utils/src/env-node/env-reader.ts
@@ -1,4 +1,5 @@
 import * as dotenv from 'dotenv'
+import { safeParseJSON } from '../lang/string.js'
 
 /**
  * 读取 .env 文件，并获取格式化后的数据
@@ -14,20 +15,44 @@ export class EnvReader {
     })
   }
 
+  protected toNumber(raw: string) {
+    const num = parseInt(raw, 10)
+    return isFinite(num) ? num : undefined
+  }
+  protected toBoolean(raw: string) {
+    const formatted = raw.toLowerCase().trim()
+    if (['1', 'true', 'on'].includes(formatted)) return true
+    if (['0', 'false', 'off'].includes(formatted)) return false
+    return undefined
+  }
+
+  getRaw(key: string) {
+    return this.envsFromFile[key] ?? process.env[key]
+  }
+
   get(key: string, defaults: string): string
   get(key: string, defaults: number): number
   get(key: string, defaults: boolean): boolean
-  get(key: string, defaults: string | number | boolean) {
-    const value = this.envsFromFile[key] ?? process.env[key]
+  get<T extends unknown[] | Record<string, unknown>>(key: string, defaults: T): T
+  get(key: string, defaults: string | number | boolean | unknown[] | Record<string, unknown>) {
+    const value = this.getRaw(key)
     if (value === undefined) return defaults
 
-    if (typeof defaults === 'number') {
-      const numValue = parseInt(value, 10)
-      return isFinite(numValue) ? numValue : defaults
-    } else if (typeof defaults === 'boolean') {
-      return ['1', 'true'].includes(value.toLowerCase().trim())
-    } else {
-      return value
-    }
+    if (typeof defaults === 'number') return this.toNumber(value) ?? defaults
+    else if (typeof defaults === 'boolean') return this.toBoolean(value) ?? defaults
+    else if (Array.isArray(defaults) || typeof defaults === 'object')
+      return safeParseJSON(value) ?? defaults
+
+    return value
+  }
+
+  getNumber(key: string) {
+    return this.toNumber(this.getRaw(key) ?? '')
+  }
+  getBoolean(key: string) {
+    return this.toBoolean(this.getRaw(key) ?? '')
+  }
+  getJSON<T>(key: string) {
+    return safeParseJSON<T>(this.getRaw(key) ?? '')
   }
 }
