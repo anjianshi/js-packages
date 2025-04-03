6dfcd34dd1285ea3cf591c07074a0b700035f708
anjianshi <anjianshi@gmail.com>
2025-04-03 19:51:21 +0800

    修正 service template 中，task 日志不显示的问题，原因是 service 的 rootLogger 和 task 的不是一套 logger

--- create/templates/service/src/logger.ts
+++ create/templates/service/src/logger.ts
@@ -1,9 +1,10 @@
-import { Logger, getLogger } from 'starlight-server'
+import { logger as rootLogger } from '@anjianshi/utils/env-node/logging/index.js'
+import { Logger, initLogger } from 'starlight-server'
 import config from '@/config.js'
 
-export { Logger }
+export { Logger, rootLogger }
 
-export const rootLogger = getLogger({
+initLogger(rootLogger, {
   level: config.DEBUG ? 'debug' : 'info',
   debugLib: '*',
   file: { dir: config.LOGS_DIR },
