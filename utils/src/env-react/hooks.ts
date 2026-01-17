2d887a027a22375b3e62e083bad8e228cbccef29
anjianshi <anjianshi@gmail.com>
2026-01-17 18:19:21 +0800

    新增 useAsyncEffect

--- utils/src/env-react/hooks.ts
+++ utils/src/env-react/hooks.ts
@@ -18,3 +18,42 @@ export function useStateWithRef<T>(initialValue: T | (() => T)) {
 
   return [state, setStateWithRef, ref] as const
 }
+
+/**
+ * 在 useEffect() 中执行异步操作。
+ * 因是异步运行，对清理机制的处理有所变化。
+ *
+ * 原本 useEffect() 中，通过返回一个函数来设置清理条件，在异步操作中不适用。
+ * 此外，同步内容是一定会在执行完只后才有可能触发清理的，但异步内容有可能在运行完之前，依赖就已经变化，触发了清理。
+ *
+ * 这里通过一个 context 对象来应对异步的情况。
+ * context.cancelled 值代表此次运行是否已被取消（清理），异步操作执行过程中如果发现此值变为 true，则可停止执行了。
+ * context.onCancelled(() => {}) 可注册一个回调，此次执行被清理时触发。多次调用仅保留最后一次的回调。
+ *
+ * 注意：需要配置 ESLint react-hooks/exhaustive-deps 规则以保证 deps 参与依赖检查
+ * 详见 https://react.dev/reference/eslint-plugin-react-hooks/lints/exhaustive-deps
+ */
+export function useAsyncEffect(
+  callback: (context: AsyncEffectContext) => Promise<void>,
+  deps: unknown[] = [],
+) {
+  useEffect(() => {
+    let onCancel: (() => void) | null = null
+    const context = {
+      cancelled: false,
+      onCancel(callback: () => void) {
+        onCancel = callback
+      },
+    }
+
+    void callback(context)
+    return () => {
+      context.cancelled = true
+      if (onCancel) onCancel()
+    }
+  }, deps) // eslint-disable-line ts-react-hooks/exhaustive-deps
+}
+export interface AsyncEffectContext {
+  cancelled: boolean
+  onCancel: (callback: () => void) => void
+}
