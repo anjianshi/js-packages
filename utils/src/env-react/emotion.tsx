651a4b555f319ae8652a86b66a39d5eb962cf51c
anjianshi <anjianshi@gmail.com>
2026-02-05 08:51:52 +0800

    调整 emotion 相关功能实现方式，避免 vite 构建后运行报错

--- utils/src/env-react/emotion.tsx
+++ utils/src/env-react/emotion.tsx
@@ -4,35 +4,24 @@
  *
  * 使用前提：
  * 1. 只支持浏览器渲染
- * 2. 安装 @emotion/serialize、@emotion/utils
- * 3. 调用 registerEmotionFunctions() 注册 @emotion/serialize 的 serializeStyles() 和 @emotion/utils 的 insertStyles() 函数
- * 4. 用 EmotionCacheProvider 包裹 App 根元素
+ * 2. 调用 getEmotionCacheProvider()，传入来自 @emotion/react 的 withEmotionCache 函数，
+ *    并用得到的 EmotionCacheProvider 组件包裹 App 根元素
  *
  * 来自：
  * https://github.com/emotion-js/emotion/issues/1853#issuecomment-623349622
  */
-import { type EmotionCache, withEmotionCache } from '@emotion/react'
-import type { CSSInterpolation, serializeStyles as serializeStylesT } from '@emotion/serialize'
-import type { insertStyles as insertStylesT } from '@emotion/utils'
+import type { EmotionCache, withEmotionCache as withEmotionCacheT } from '@emotion/react'
+import { type CSSInterpolation, serializeStyles } from '@emotion/serialize'
+import { insertStyles } from '@emotion/utils'
 import { createContext, useContext, useCallback } from 'react'
 
 const CacheContext = createContext<EmotionCache | undefined>(undefined)
 export const useEmotionCache = () => useContext(CacheContext)
 
-export const EmotionCacheProvider = withEmotionCache(
-  ({ children }: { children: React.ReactNode }, cache: EmotionCache) => {
+export function getEmotionCacheProvider(withEmotionCache: typeof withEmotionCacheT) {
+  return withEmotionCache(({ children }: { children: React.ReactNode }, cache: EmotionCache) => {
     return <CacheContext.Provider value={cache}>{children}</CacheContext.Provider>
-  },
-)
-
-let serializeStyles: typeof serializeStylesT | null = null
-let insertStyles: typeof insertStylesT | null = null
-export function registerEmotionFunctions(
-  serializeStylesFn: typeof serializeStyles,
-  insertStylesFn: typeof insertStyles,
-) {
-  serializeStyles = serializeStylesFn
-  insertStyles = insertStylesFn
+  })
 }
 
 export function useEmotionClassName(): (...args: CSSInterpolation[]) => string {
@@ -45,13 +34,6 @@ export function useEmotionClassName(): (...args: CSSInterpolation[]) => string {
           return 'emotion-cache-missing'
         }
         throw new Error('No emotion cache found!')
-      }
-
-      if (!serializeStyles || !insertStyles) {
-        console.error(
-          'useEmotionClassName: need register serializeStyles() and insertStyles() functions first.',
-        )
-        return 'emotion-functions-missing'
       }
 
       const serialized = serializeStyles(args, cache.registered)
